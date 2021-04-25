3d8b155847ac959e279516c184de68ec528787f3
Denica Gateva <80250613+denica2324@users.noreply.github.com>
2021-04-25 10:22:53 +0300

    Initial commit

---
+++ src/Hero.js
@@ -0,0 +1,16 @@
+﻿import React from "react";
+
+
+
+const Hero = ({ handleLogOut }) => {
+    return (
+        <div>
+            
+            <button className="LogOut" onClick={handleLogOut}>
+                Log Out
+            </button>
+        </div>
+    );
+};
+
+export default Hero;
---
+++ src/fire.js
@@ -0,0 +1,15 @@
+﻿import firebase from 'firebase'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCnrR4Yy0bXrJ94yNmI1UODJbiT9sPtbI0",
+    authDomain: "login-7ace1.firebaseapp.com",
+    databaseURL: "https://login-7ace1-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "login-7ace1",
+    storageBucket: "login-7ace1.appspot.com",
+    messagingSenderId: "950362401203",
+    appId: "1:950362401203:web:a9b614a1bd100b074743c6"
+};
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
